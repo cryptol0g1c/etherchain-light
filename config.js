ceb027f7c7e07efdfd877a7cc79d86c11d2681f4
Maxi Dev <maxi.canellas@gmail.com>
2018-03-26 21:57:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ var net = require('net');
 var config = function () {
   
   this.logFormat = "combined";
-  this.ipcPath = "/home/admin/.local/share/io.parity.ethereum/jsonrpc.ipc";
+  this.ipcPath = process.env["HOME"] + "/.local/share/io.parity.ethereum/jsonrpc.ipc"
   this.provider = new web3.providers.IpcProvider(this.ipcPath, net);
   this.bootstrapUrl = "https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/paper/bootstrap.min.css";
   this.names = {
